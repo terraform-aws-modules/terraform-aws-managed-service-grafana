4bf72a072959732fc2208e393b7772fa72e18cf9
nesty92 <ernesto.alejandrosantana@gmail.com>
2024-02-01 12:36:48 +0100

    Fix prometheus alerts integration policies

--- main.tf
+++ main.tf
@@ -283,7 +283,7 @@ data "aws_iam_policy_document" "this" {
         "aps:GetAlertManagerStatus",
         "aps:ListAlertManagerAlertGroups",
         "aps:PutAlertManagerSilences",
-        "aps:DeleteAlertManagerSilences"
+        "aps:DeleteAlertManagerSilence"
       ]
       resources = ["*"]
     }
